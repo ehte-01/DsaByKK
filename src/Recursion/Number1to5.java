ab1e864c3618e298b946c3da09dde615b5995230
MOHAMMAD EHTESHAM <infoehtesham7886@gmail.com>
2025-11-08 02:18:38 +0530

    DsaByKK

---
+++ src/Recursion/Number1to5.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+public class Number1to5 {
+    public static void main(String[] args) {
+        print(1);
+    }
+
+    static void print(int n) {
+        //Base condition
+        if (n == 5) {
+            System.out.println(5);
+            return;
+        }
+        System.out.println(n);
+        print(n+1);
+    }
+}
