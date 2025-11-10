fcb21ac3e3845cb1c5730a60401c5602f47e64a2
MOHAMMAD EHTESHAM <infoehtesham7886@gmail.com>
2025-11-10 20:30:20 +0530

    DsaByKK

--- src/Recursion/RotatedBinarySearch.java
+++ src/Recursion/RotatedBinarySearch.java
@@ -3,7 +3,7 @@ package Recursion;
 public class RotatedBinarySearch {
     public static void main(String[] args) {
         int[] arr={5,6,7,8,9,1,2,3};
-        System.out.println(search(arr,1,0,arr.length-1));
+        System.out.println(search(arr,3,0,arr.length-1));
     }
 
     static int search(int[] arr, int target ,int s ,int e){
