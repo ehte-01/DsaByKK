bc5264d38eee65e91332380e463e4142eb73db2d
MOHAMMAD EHTESHAM <infoehtesham7886@gmail.com>
2025-11-12 23:15:44 +0530

    DsaByKK

--- src/Recursion/Sorting/SelectionSort.java
+++ src/Recursion/Sorting/SelectionSort.java
@@ -2,6 +2,33 @@ package Recursion.Sorting;
 
 public class SelectionSort {
     public static void main(String[] args) {
+        int[] arr = {5, 3, 6, 2, 1, 4};
+        sort(arr);
 
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    static void sort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i; // Assume the current index is the smallest
+
+            // Find the minimum element in the remaining unsorted array
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap the found minimum element with the first element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
     }
 }
+
