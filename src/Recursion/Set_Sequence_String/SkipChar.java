4a9cc65d358033e348ef68ce7565bc1e70780a2f
MOHAMMAD EHTESHAM <infoehtesham7886@gmail.com>
2025-11-18 22:19:19 +0530

    DsaByKK

---
+++ src/Recursion/Set_Sequence_String/SkipChar.java
@@ -0,0 +1,23 @@
+package Recursion.Set_Sequence_String;
+
+public class SkipChar {
+    public static void main(String[] args) {
+        skip("", "baccad", 'a');
+    }
+
+    static void skip(String processed, String unprocessed, char target) {
+        if(unprocessed.isEmpty()) {
+            System.out.println(processed);
+            return;
+        }
+
+        char ch = unprocessed.charAt(0);
+
+        if(ch == target) {
+            skip(processed, unprocessed.substring(1), target);
+        } else {
+            skip(processed + ch, unprocessed.substring(1), target);
+        }
+    }
+}
+
