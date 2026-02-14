1e6a5c8608dbb1d9c42a2210d279b3c69a70542c
MOHAMMAD EHTESHAM <infoehtesham7886@gmail.com>
2026-02-14 23:05:13 +0530

    DsaByKunalKushwaha

---
+++ src/LinkedList/DDLMain.java
@@ -0,0 +1,13 @@
+package LinkedList;
+
+public class DDLMain {
+
+    public static void main(String[] args) {
+
+        DDL ddl = new DDL();
+        ddl.insertFirst(4);
+        ddl.insertFirst(5);
+        ddl.insertFirst(6);
+        ddl.display();
+    }
+}
