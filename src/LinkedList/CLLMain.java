0b57f6d39ada19f0a722fff947a572c425358936
MOHAMMAD EHTESHAM <infoehtesham7886@gmail.com>
2026-02-15 16:28:46 +0530

    DsaByKunalKushwaha

---
+++ src/LinkedList/CLLMain.java
@@ -0,0 +1,16 @@
+package LinkedList;
+
+public class CLLMain {
+    public static void main(String[] args) {
+
+        CLL cll = new CLL();
+        cll.insert(23);
+        cll.insert(34);
+        cll.insert(45);
+        cll.insert(65);
+        cll.display();
+        cll.delete(34);
+        cll.display();
+
+    }
+}
